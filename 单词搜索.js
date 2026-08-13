9adedef1600972e4c0d30bc12b150d5014add6f5
songyifan <yifansong@bytedance.com>
2026-08-13 19:42:50 +0800

    feat: 新增单词搜索题解 + 回溯笔记补齐网格类(C类)
    - 单词搜索: 四方向 DFS + 盖章防重踩 + 返回值抬回顶层
    - 回溯笔记新增第十二节: 网格回溯为何难、五步模板、x/y 坐标约定、实战坑
    - 家族地图新增 C 类, README 题解索引同步

--- 单词搜索.js
+++ 单词搜索.js
@@ -4,5 +4,30 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    
+    const n = board.length;
+    const m = board[0].length;
+
+    const dfs = (x, y, index) => {
+        if(x < 0 || x >= m || y < 0 || y >= n) {
+            return false;
+        }
+        if(word[index] != board[y][x]) {
+            return false;
+        }
+        if(index === word.length - 1) return true;
+        
+        board[y][x] = '#';
+        const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        for(const [i, j] of dirs) {
+            if(dfs(x + j, y + i, index + 1)) return true;
+        }
+        board[y][x] = word[index];
+        return false;
+    }
+    for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) {
+               if(dfs(j, i, 0) ) return true;
+            }
+        }
+    return false;
 };
